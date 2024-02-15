942d15048284169a2a0815338b7a604adb23b042
ylked <nima@dekhli.ch>
2024-02-15 20:47:39 +0100

    add header and footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-footer
+    class="text-center d-flex flex-column"
+    app
+  >
+    <div>
+      Copyright &copy; {{ new Date().getFullYear() }} <strong>Haute Ecole Arc</strong>.
+      <a href="https://github.com/HE-Arc/neurona/blob/main/LICENSE">Licensed under the MIT License.</a>
+    </div>
+
+  </v-footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,112 @@
+<script setup>
+
+import {ref} from 'vue';
+import {useDisplay} from 'vuetify';
+
+const drawer = ref(false);
+const items = [
+  {title: 'Home', icon: 'mdi-home', to: '/'},
+  {title: 'Saved', icon: 'mdi-bookmark', to: '/saved'},
+];
+
+const profile = [
+  {title: 'Settings', icon: 'mdi-cog', to: '/settings'},
+  {title: 'About us', icon: 'mdi-information', to: '/about'},
+  {title: 'Logout', icon: 'mdi-logout', to: '/logout'},
+];
+
+const bottom_items = [
+  {title: 'Settings', icon: 'mdi-cog', to: '/settings'},
+  {title: 'About us', icon: 'mdi-information', to: '/about'},
+  {title: 'Logout', icon: 'mdi-logout', to: '/logout'},
+]
+
+const spaces = [
+  {title: 'ISC1', avatar: 'mdi-account', id: 1, to: 'spaces/1'},
+  {title: 'ISC2', avatar: 'mdi-account', id: 2, to: 'spaces/2'},
+]
+
+const is_mobile = useDisplay().smAndDown;
+
+drawer.value = !is_mobile.value;
+
+</script>
+
+<template>
+  <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+
+  <v-app-bar
+    color="primary"
+    prominent
+    app
+  >
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-toolbar-title>
+      {{ $route.meta.title }}
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+    :permanent="!is_mobile"
+    :temporary="is_mobile"
+  >
+    <v-list nav>
+      <v-list-item to="/profile" value="profile">
+        <template v-slot:prepend>
+          <v-avatar image="https://nzbirdsonline.org.nz/sites/all/files/2X2A1697%20King%20Penguin%20bol.jpg"></v-avatar>
+        </template>
+        <v-list-item-title>John Doe</v-list-item-title>
+      </v-list-item>
+
+      <v-divider/>
+
+      <v-list-item
+        v-for="(item, index) in items"
+        :key="index"
+        :value="item"
+        :to="item.to"
+      >
+        <template v-slot:prepend>
+          <v-icon>{{ item.icon }}</v-icon>
+        </template>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+
+      <v-divider/>
+
+      <v-list-item
+        v-for="(item, index) in spaces"
+        :to="item.to"
+        :key="index"
+        :value="item.id"
+        :prepend-avatar="item.avatar"
+        nav>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+
+      <v-divider/>
+      <v-list-item
+        v-for="(item, index) in bottom_items"
+        :key="index"
+        :value="item"
+        :to="item.to">
+        <template v-slot:prepend>
+          <v-icon>{{ item.icon }}</v-icon>
+        </template>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<style scoped>
+
+</style>
