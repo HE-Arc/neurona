35711c019da4a2d68bd399af0cff686fbaa49ff3
ylked <nima@dekhli.ch>
2024-02-20 10:31:14 +0100

    add return button component

---
+++ frontend/src/components/ReturnBtn.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import router from "@/router";
+</script>
+
+<template>
+  <v-btn
+      prepend-icon="mdi-arrow-left"
+      class="ma-5"
+      @click="router.go(-1)"
+  >
+    Back
+  </v-btn>
+</template>
+
+<style scoped>
+
+</style>
