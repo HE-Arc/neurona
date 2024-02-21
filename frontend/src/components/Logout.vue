bbaec5a677510f14ad4a14a4f57f522f4e86e6ab
ylked <nima@dekhli.ch>
2024-02-21 17:05:12 +0100

    add logout view

---
+++ frontend/src/components/Logout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import router from "@/router";
+import {set_current_auth_state, state} from "@/Authentication/store";
+
+state.authenticated = false;
+set_current_auth_state('false');
+router.push({name: 'login'});
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
