e3ab38534f444cb8596d19d5a896c7e40efa1fb6
Nima Dekhli <nima@dekhli.ch>
2024-03-15 11:32:25 +0100

    improve space view

--- api/neuronaApp/views/space_view.py
+++ api/neuronaApp/views/space_view.py
@@ -4,20 +4,18 @@ from rest_framework import status
 from ..models import Spaces
 from ..serializers import SpaceSerializer
 
-# Get all spaces
-
 
 @api_view(['GET'])
 def get_spaces(request):
+    """Get all spaces."""
     spaces = Spaces.objects.all()
     serializer = SpaceSerializer(spaces, many=True)
     return Response(serializer.data)
 
-# Get specific space by id
-
 
 @api_view(['GET'])
 def get_space(request, pk):
+    """Get a specific space by id."""
     try:
         space = Spaces.objects.get(pk=pk)
         serializer = SpaceSerializer(space)
@@ -26,28 +24,26 @@ def get_space(request, pk):
     except Spaces.DoesNotExist:
         return Response({'error': 'Space not found'}, status=status.HTTP_404_NOT_FOUND)
 
-# Create a space
-
 
 @api_view(['POST'])
 def create_space(request):
+    """Create a new space."""
     serializer = SpaceSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-# Update a space by id, either with put or patch method
 
 
 @api_view(['PUT', 'PATCH'])
 def update_space(request, pk):
+    """Update a space by id with either put or patch method."""
     try:
         space = Spaces.objects.get(pk=pk)
         if request.method == 'PUT':
             serializer = SpaceSerializer(space, data=request.data)
-        elif request.method == 'PATCH':
+        else:
             serializer = SpaceSerializer(
                 space, data=request.data, partial=True)
 
@@ -61,11 +57,9 @@ def update_space(request, pk):
         return Response({'error': 'Space not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
-# Delete a space by id
-
-
 @api_view(['DELETE'])
 def delete_space(request, pk):
+    """Delete a space by id."""
     try:
         space = Spaces.objects.get(pk=pk)
         space.delete()
@@ -73,4 +67,3 @@ def delete_space(request, pk):
 
     except Spaces.DoesNotExist:
         return Response({'error': 'Space not found'}, status=status.HTTP_404_NOT_FOUND)
-
