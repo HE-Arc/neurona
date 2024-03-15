5a0d0f8b13f0d33785107e9693bec44bca640068
Nima Dekhli <nima@dekhli.ch>
2024-03-15 12:39:04 +0100

    Update api/neurona/settings.py

--- api/neurona/settings.py
+++ api/neurona/settings.py
@@ -55,19 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-
-]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-]
-
-CORS_ALLOW_ALL_ORIGINS = True #pour les tests
-
-
-
 ROOT_URLCONF = "neurona.urls"
 
 TEMPLATES = [
