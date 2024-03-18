ce37bb043e03d448eb8729dd3e4b12a626906932
Nima Dekhli <nima@dekhli.ch>
2024-03-18 18:05:22 +0100

    get debug mode from environment variable (#37)

--- api/neurona/settings.py
+++ api/neurona/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-u2y&tl=67+qayf=6zphl*8$&sygjuik@-1wux%84xf2b*)dq$t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
