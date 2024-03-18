ed3f05ec58d72f5c674807093b36d5ad2eedddf8
Nima Dekhli <nima@dekhli.ch>
2024-03-18 18:33:38 +0100

    add authentication token class (#39)

---
+++ api/neuronaApp/token_authentication.py
@@ -0,0 +1,24 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+from neuronaApp.models import ApiKeys
+
+
+class TokenAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.META.get("HTTP_AUTHORIZATION")
+
+        if not token:
+            return None
+
+        try:
+            token = ApiKeys.objects.get(key=token)
+
+        except ApiKeys.DoesNotExist:
+            raise AuthenticationFailed("Invalid token")
+
+        if token.has_expired():
+            token.delete()
+            raise AuthenticationFailed("Token has expired")
+
+        return token.user, token
