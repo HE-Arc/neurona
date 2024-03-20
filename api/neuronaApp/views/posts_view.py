66e15e233f2750f32baedb47b686e777ce380471
Nima Dekhli <nima@dekhli.ch>
2024-03-20 21:10:39 +0100

    update posts view

--- api/neuronaApp/views/posts_view.py
+++ api/neuronaApp/views/posts_view.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from neuronaApp.models import Posts, Comments
-from neuronaApp.serializers import PostsSerializer, CommentsSerializer
+from rest_framework.response import Response
 
-class PostsViewSet(viewsets.ModelViewSet):
+from neuronaApp.models import Posts, Comments
+from neuronaApp.serializers import PostsSerializer, CommentsSerializer, UserSerializer, PostsUserSerializer
+from neuronaApp.token_authentication import TokenAuthentication
+from neuronaApp.views.authentication_view import logger
+
+
+class PostsViewSet(viewsets.ViewSet):
     queryset = Posts.objects.all()
-    serializer_class = PostsSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def list(self, request, *args, **kwargs):
+        queryset = Posts.objects.all()
+        serializer = PostsUserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data.copy()
+        data['user'] = user.id
+        serializer = PostsSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
