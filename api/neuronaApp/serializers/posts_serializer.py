a9e6bb8a8514906f65af45f5d6fb902b33482f81
Nima Dekhli <nima@dekhli.ch>
2024-03-20 21:09:33 +0100

    update api serializers

--- api/neuronaApp/serializers/posts_serializer.py
+++ api/neuronaApp/serializers/posts_serializer.py
@@ -1,10 +1,30 @@
 from rest_framework import serializers
 from neuronaApp.models import Posts, Comments, PostsImages, CommentsImages, Votes, CommentsVotes
+from neuronaApp.serializers.users_serializer import UserSerializer
+
 
 class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
-        fields = '__all__'
+        fields = [
+            "user",
+            "title",
+            "content",
+            "space",
+            "tag",
+        ]
+
+class PostsUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Posts
+        fields = [
+            "user",
+            "title",
+            "content",
+            "space",
+            "tag",
+        ]
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
