ae5bf808237f8c9b938a9dd2a86367d0ac19be77
Nima Dekhli <nima@dekhli.ch>
2024-04-16 15:24:49 +0200

    add backend to edit profile

--- api/neuronaApp/serializers/users_serializer.py
+++ api/neuronaApp/serializers/users_serializer.py
@@ -5,6 +5,8 @@ from rest_framework import serializers
 from neuronaApp.models import User
 
 DEFAULT_AVATAR_BASE_URL = "https://avatar.iran.liara.run/username?username="
+BIOGRAPHY_MAX_LENGTH = 200
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,3 +25,15 @@ class UserSerializer(serializers.ModelSerializer):
             display_name_encoded = urllib.parse.quote_plus(data["display_name"])
             data["image_url"] = DEFAULT_AVATAR_BASE_URL + display_name_encoded
         return data
+
+
+class BiographySerializer(serializers.Serializer):
+    about = serializers.CharField(max_length=200)
+
+    def validate_about(self, value):
+        if len(value) > BIOGRAPHY_MAX_LENGTH:
+            raise serializers.ValidationError(f"Biography must be at most {BIOGRAPHY_MAX_LENGTH} characters long")
+        return value
+
+    def get_error_message(self):
+        return self.errors["about"][0]
--- api/neuronaApp/views/user_profile_view.py
+++ api/neuronaApp/views/user_profile_view.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from neuronaApp.serializers import UsernameSerializer, EmailSerializer, UserSerializer
+from neuronaApp.serializers import UsernameSerializer, EmailSerializer, UserSerializer, DisplayNameSerializer
+from neuronaApp.serializers.users_serializer import BiographySerializer
 from neuronaApp.token_authentication import TokenAuthentication
 
+
+from neuronaApp.views.authentication_view import logger
 
 class Validity(viewsets.ViewSet):
     @action(detail=False, methods=["GET"])
@@ -33,3 +36,42 @@ class Profile(viewsets.ViewSet):
     def list(self, request, *args, **kwargs):
         user = request.user
         return Response(UserSerializer(user).data)
+
+    @action(detail=False, methods=["PUT"])
+    def username(self, request, *args, **kwargs):
+        user = request.user
+        serializer = UsernameSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response({"message": serializer.get_error_message()}, status=400)
+
+        user.username = serializer.validated_data["username"]
+        user.save()
+
+        return Response(status=200)
+
+    @action(detail=False, methods=["PUT"])
+    def display_name(self, request, *args, **kwargs):
+        user = request.user
+        serializer = DisplayNameSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response({"message": serializer.get_error_message()}, status=400)
+
+        user.display_name = serializer.validated_data["display_name"]
+        user.save()
+
+        return Response(status=200)
+
+    @action(detail=False, methods=["PUT"])
+    def about(self, request, *args, **kwargs):
+        user = request.user
+        serializer = BiographySerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response({"message": serializer.get_error_message()}, status=400)
+
+        user.about = serializer.validated_data["about"]
+        user.save()
+
+        return Response(status=200)
