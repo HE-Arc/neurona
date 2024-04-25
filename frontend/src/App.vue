c28173c69c007ac84148e830c8de82415d3f557e
mathias <mathias.salmon@he-arc.ch>
2024-04-25 21:40:18 +0200

    fetch posts and spaces when user is logged-in

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,22 +11,12 @@
 
 <script setup lang="ts">
 //
-import { onBeforeMount } from "vue";
 import Navbar from "@/components/Navbar.vue";
 import AlertBanner from "@/components/alerts/AlertBanner.vue";
 import MessageManager from "@/tools/MessageManager";
 import SnackbarMessage from "@/components/alerts/SnackbarMessage.vue";
-import { useSpaceStore } from "@/stores/SpaceStore";
-import { usePostStore } from "@/stores/PostStore";
 
 const messages = MessageManager.getInstance().get();
 const snack = MessageManager.getInstance().getSnackbar();
-
-onBeforeMount(() => {
-  const spaceStore = useSpaceStore();
-  const postStore = usePostStore();
-  spaceStore.fetchSpaces();
-  postStore.fetchPosts();
-});
 
 </script>
