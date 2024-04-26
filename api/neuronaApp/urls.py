cf0996c32bc659fe5989972d415c088906876e19
Nima Dekhli <nima@dekhli.ch>
2024-04-26 11:13:38 +0200

    refactor : use viewsets for space views instead of apiView in api

--- api/neuronaApp/urls.py
+++ api/neuronaApp/urls.py
@@ -10,17 +10,13 @@ router.register(r'passkey-options', views.PasskeyChallengeView, basename='passke
 router.register(r'posts', PostsViewSet)
 router.register(r'comments', CommentsViewSet, basename='comments')
 router.register(r'profile', views.Profile, basename='profile')
+router.register(r'spaces', views.SpacesViewSet, basename='spaces')
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("", include(router.urls)),
-    path('spaces/', views.space_view.get_spaces, name='get_spaces'),
-    path('spaces/<int:pk>/', views.space_view.get_space, name='get_space'),
-    path('spaces/', views.space_view.create_space, name='create_space'),
-    path('spaces/<int:pk>/', views.space_view.update_space, name='update_space'),
-    path('spaces/<int:pk>/', views.space_view.delete_space, name='delete_space'),
     path('posts/<int:pk>/upvote/', views.VoteView.as_view(actions={'post': 'upvote'}), name='upvote'),
     path('posts/<int:pk>/downvote/', views.VoteView.as_view(actions={'post': 'downvote'}), name='downvote'),
     path('posts/<int:pk>/unvote/', views.VoteView.as_view(actions={'post': 'unvote'}), name='unvote'),
