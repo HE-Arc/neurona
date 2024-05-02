b7b03fe33fe5cb0bb359d662b2fe67ec679fab20
Nima Dekhli <nima@dekhli.ch>
2024-05-03 01:49:11 +0200

    add is_admin field to space response

--- api/neuronaApp/serializers/space_serializer.py
+++ api/neuronaApp/serializers/space_serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from ..models import Spaces, SpacesMembers
+from ..models import Spaces, SpacesMembers, SpacesAdmins
 from ..models import Privacy
  
 class SpaceSerializer(serializers.ModelSerializer):
     joined = serializers.SerializerMethodField()
+    is_admin = serializers.SerializerMethodField()
 
     class Meta:
         model = Spaces
@@ -12,8 +13,12 @@ class SpaceSerializer(serializers.ModelSerializer):
             'name',
             'about',
             'image_url',
-            'joined'
+            'joined',
+            'is_admin',
         ]
 
     def get_joined(self, obj):
         return SpacesMembers.objects.filter(user_id=self.context.id, space_id=obj.id).exists()
+
+    def get_is_admin(self, obj):
+        return SpacesAdmins.objects.filter(user_id=self.context.id, space_id=obj.id).exists()
